0db8cdd58cf4dd4c17d38166f5ce1c3709e44f94
Douglas Jara Negrete <douglasjara@gmail.com>
2022-05-26 11:42:26 +0200

    #2 Stores characters list in local (only the first API call), and allows retrieve that data from the device

--- marvel-app/Models/Services/CharactersService.swift
+++ marvel-app/Models/Services/CharactersService.swift
@@ -11,34 +11,81 @@ import CryptoKit
 class CharactersService
 {
     private(set) var characterDataWrapper = CharacterDataWrapper()
-    weak var delegate: CharactersServiceDelegate?    
+    weak var delegate: CharactersServiceDelegate?
+    private let fileURL: URL
+    private let fileManager = FileManager.default
+    private var localRequests = 0
     
     init()
     {
-        debugPrint("Init: Getting data from API!")
-        getCharactersFromAPI(limit: MarvelServiceSettings.defaultLimit, offset: MarvelServiceSettings.defaultOffset)
+        let fileName = "characters"
+        let documentsURL =  fileManager.urls(for: .documentDirectory, in: .userDomainMask).first!
+        let directoryURL = documentsURL.appendingPathComponent("Marvel", isDirectory: true)
+        self.fileURL = directoryURL.appendingPathComponent("\(fileName).json", isDirectory: false)
+        
+        debugPrint("Init: Getting data!")
+        getData(limit: MarvelServiceSettings.defaultLimit, offset: MarvelServiceSettings.defaultOffset)
     }
     
     public func refreshData()
     {
-        debugPrint("Refreshing data from API!")
-        getCharactersFromAPI(limit: MarvelServiceSettings.defaultLimit, offset: MarvelServiceSettings.defaultOffset)
+        debugPrint("Refreshing data!")
+        getData(limit: MarvelServiceSettings.defaultLimit, offset: MarvelServiceSettings.defaultOffset)
     }
     
     public func getData(limit: Int, offset: Int)
     {
-        debugPrint("Getting data from API!")
-        getCharactersFromAPI(limit: limit, offset: offset)
+        if MarvelServiceSettings.getOnlineData {
+            getCharactersFromAPI(limit: limit, offset: offset)
+        } else {
+            getCharactersFromLocal()
+        }
+    }
+    
+    private func getCharactersFromLocal()
+    {
+        //TODO: Pagination in local
+        if self.localRequests == 0 {
+            debugPrint("Getting data from LOCAL!")
+            DispatchQueue.main.async {
+                if let charactersFileContents = self.fileManager.contents(atPath: self.fileURL.path) {
+                    do {
+                        let characterDataWrapper = try JSONDecoder().decode(CharacterDataWrapper.self, from: charactersFileContents)
+                        self.characterDataWrapper = characterDataWrapper
+                        self.delegate?.charactersServiceDelegateDidUpdate(self, append: false)
+                        self.localRequests += 1
+                    } catch {
+                        debugPrint("Can't retrieve characters data from file")
+                    }
+                } else {
+                    debugPrint("No contents")
+                }
+            }
+        }
     }
     
     private func getCharactersFromAPI(limit: Int, offset: Int)
     {
+        debugPrint("Getting data from API!")
         let service = APIManager(url: getMarvelUrl(limit: limit, offset: offset))
         service.get() { (characterDataWrapper: CharacterDataWrapper?) in
             if let characterDataWrapper = characterDataWrapper {
                 self.characterDataWrapper = characterDataWrapper
                 if (offset == MarvelServiceSettings.defaultOffset) {
                     self.delegate?.charactersServiceDelegateDidUpdate(self, append: false)
+                    
+                    let data = try! JSONEncoder().encode(characterDataWrapper)
+                    
+                    do {
+                        let directory = self.fileURL.deletingLastPathComponent()
+                        try? FileManager.default.removeItem(at: directory)
+                        try? FileManager.default.createDirectory(at: directory, withIntermediateDirectories: true)
+                        try data.write(to: self.fileURL, options: .atomic)
+                        
+                        debugPrint("Directorio Marvel creado: \(self.fileURL)")
+                    } catch let fileError {
+                        debugPrint("\(CharactersService.self): (getCharactersFromAPI) Error creating file: \(fileError)")
+                    }
                 } else {
                     self.delegate?.charactersServiceDelegateDidUpdate(self, append: true)
                 }
